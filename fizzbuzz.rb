2f9a4188a5916bb53a7e53686f3ad51134dadd50
hpaulini <helenpaulini@gmail.com>
2018-07-10 19:03:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,12 +5,12 @@ def fizzbuzz(int)
     if int % 3 == 0 && int % 5 == 0
       "FizzBuzz"
    
-elsif int % 3 == 0
-   "Fizz"
+    elsif int % 3 == 0
+       "Fizz"
    
-elsif int % 5 == 0
-   "Buzz"
-end
+    elsif int % 5 == 0
+       "Buzz"
+    end
 
 end
 
